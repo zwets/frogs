6aa60ce0631aeee98998d99a0e31342cdbc8e603
Marco van Zwetselaar <io@zwets.it>
2022-09-04 02:24:34 +0300

    [11-reads-dtu-trim] add links

--- 11-reads-dtu-trim/make-links.sh
+++ 11-reads-dtu-trim/make-links.sh
@@ -4,7 +4,9 @@ LC_ALL=C
 set -euo pipefail
 cd "$(dirname "$0")"
 
-IN_DIR='/hpc/data/genomics/kcri/reads/raw/QDK21'
+IN_DIR='/hpc/data/genomics/kcri/reads/trimmed/QDK21'
+
+rm -f *.fq.gz
 
 for F in $IN_DIR/*.fq.gz; do
     [ -f "$F" ] || continue
