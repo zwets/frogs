0ce839cd5419764cbfbdee4bb43e2c37a7dc16bf
Marco van Zwetselaar <io@zwets.it>
2022-09-05 16:00:11 +0300

    [41-mito-map] fix headers

--- 41-mito-map-kma/extract-seqs.sh
+++ 41-mito-map-kma/extract-seqs.sh
@@ -7,6 +7,6 @@ for F in MAP_*_{029,111}/*.fsa; do
     L=$(uf "$F" | uf-bare | wc -c)
     (( L > 15000 )) || continue
     N="$(dirname "$F" | sed -Ee 's/MAP_//;s/_029/-MT-KA/;s/_111/-MT-KB/').fna"
-    I="$(echo "$N" | sed -Ee 's/-K.\.fna//')"
+    I="$(echo "$N" | sed -Ee 's/\.fna//')"
     uf "$F" | sed -Ee '1s/>.*/>'$I'/' >"$N"
 done
