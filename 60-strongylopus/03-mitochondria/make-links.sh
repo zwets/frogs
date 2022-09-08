748294a4045517f723886e8a6042e6a5a7997af3
Marco van Zwetselaar <io@zwets.it>
2022-09-08 22:11:03 +0300

    [60-strongy/10-mito-align] added

---
+++ 60-strongylopus/03-mitochondria/make-links.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for S in 028 029 097 111 120; do
+    ln -sf ../../42-mito-map-all/DZ${S}-MT-K{A,B}.fna .
+done
+
---
+++ 60-strongylopus/10-mito-align/get-unaligned.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export LC_ALL="C"
+set -euo pipefail
+
+uf ../03-mitochondria/*.fna >unaligned.fna
+
